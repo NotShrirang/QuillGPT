ecfa1f671eadef19ada00397d1a654d6e235a83f
NotShrirang <shrirangmahajan123@gmail.com>
2024-04-21 16:16:42 +0530

    fix: train_gpt.py script

--- scripts/train_gpt.py
+++ scripts/train_gpt.py
@@ -1,7 +1,8 @@
-import tqdm
-import torch
-import sys
+import json
 import os
+import sys
+import torch
+import tqdm
 
 # Get the parent directory of the current file
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,15 +14,23 @@ sys.path.append(parent_dir)
 import utils.gptutils as gptutils
 from core.models.gpt import GPTLanguageModel
 
-torch.manual_seed(0)
+config_path = os.path.join(parent_dir, 'config/config.json')
+with open(config_path) as f:
+    config = json.load(f)
+data_path = os.path.join(parent_dir, config['data_path'])
+name = "GPT"
 
-hyperparameters = gptutils.hyperparameters()
+print("Loading data...")
+train_data, val_data, vocab_size, encode, decode = gptutils.load_data(data_path)
+print(f"\nData loaded from `{data_path}`. Vocab size: {vocab_size}.")
+
 (batch_size, block_size, max_iters, eval_interval, learning_rate, device,
- eval_iters, n_embd, n_head, n_layer, dropout, vocab_size) = hyperparameters
+    eval_iters, n_embd, n_head, n_layer, dropout, vocab_size) = gptutils.hyperparameters(config_path=config_path, data_path=data_path)
+
 
 print("Loading model...")
-model = GPTLanguageModel()
-m = model.to(device)
+model = GPTLanguageModel(vocab_size, n_embd, block_size, n_head, n_layer, dropout, device, name)
+model = model.to(device)
 print("Loaded model on device `{}`".format(device))
 print("""Hyperparameters:
     batch_size: {}
@@ -38,20 +47,20 @@ print("""Hyperparameters:
 """.format(batch_size, block_size, max_iters, eval_interval, learning_rate,
            eval_iters, n_embd, n_head, n_layer, dropout, vocab_size))
 print("\nModel is of ", sum(p.numel()
-      for p in m.parameters())/1e6, 'M parameters')
+      for p in model.parameters())/1e6, 'M parameters')
 
-optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 print("\nStarting training...")
 for iter in tqdm.tqdm(range(max_iters)):
     if not iter == 0:
         if iter % eval_interval == 0 or iter == max_iters - 1:
-            losses = gptutils.estimate_loss(model)
+            losses = gptutils.estimate_loss(model, gptutils.get_batch, eval_iters)
             print(
                 f"step {iter}: train loss {losses['train']:.4f}, val loss \
                     {losses['val']:.4f}")
 
-    xb, yb = gptutils.get_batch('train')
+    xb, yb = gptutils.get_batch('train', train_data, val_data, device, block_size, batch_size)
 
 
     logits, loss = model(xb, yb)
@@ -63,7 +72,7 @@ print("\nTraining complete.")
 
 print("Generating text...")
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(gptutils.decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(gptutils.decode(model.generate(context, max_new_tokens=100)[0].tolist()))
 
 # save the model
 os.makedirs('./trained_models', exist_ok=True)
